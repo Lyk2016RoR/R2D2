1ddb906e3599408c07f8e2fc41c28b2a39c35920
Mehmet Arı <memojja1907@gmail.com>
2016-08-18 19:25:52 +0300

    Authoer added

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :categories
+  resources :authors
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
